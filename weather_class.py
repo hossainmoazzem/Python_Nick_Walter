3c9874a278879270e2459fcd2788bf2e1ce14691
Moazzem Hossain <48817696+hossainmoazzem@users.noreply.github.com>
2024-06-03 10:56:32 +0000

    tested with 3 cities, London, Sydney and Dhaka

--- weather_class.py
+++ weather_class.py
@@ -1,20 +1,38 @@
 import requests
 
-
-class Weather:
-    def __init__(self, lat, lon, units = "metric"):
+class City:
+    def __init__(self, name, lat, lon, units = "metric"):
+        self.name = name
         self.lat = lat
         self.lon = lon
         self.units = units
+        self.get_data()
 
-weather_info = requests.get(f"https://api.openweathermap.org/data/2.5/weather?{self.lat}&{self.lon}&appid=7fd7eefeb1dc04c071716e91b1567e02&units=metric")
-# print(weather_info)
-weather_info_json = weather_info.json()
-temp = weather_info_json["main"]["temp"]
-print(temp)
-# print(weather_info)
-# print(weather_info.json()) 
+    def get_data(self):
+        weather_info = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid=7fd7eefeb1dc04c071716e91b1567e02&units={self.units}")
+        weather_info_json = weather_info.json()
+        self.temp = weather_info_json["main"]["temp"]
+        self.temp_max = weather_info_json["main"]["temp_max"]
+        self.temp_min = weather_info_json["main"]["temp_min"]
+
+        
+    def temp_print(self):
+        print(f"The Temperature in your city Currently: {self.temp}° C")
+        print(f"The Highest Temperature in your city today: {self.temp_max}° C")
+        print(f"The Lowest Temperature in your city today: {self.temp_min}° C")
+        print("-------------------------------------------------------")
 
 
 
-my_city = Weather(self.lat,self.lon)
+
+
+London = City("London",51.509865,-0.118092)
+London.temp_print()
+
+Sydney = City("Sydney",-33.968109,151.104080)
+Sydney.temp_print()
+
+Dhaka = City("Dhaka",23.822350,90.365417)
+Dhaka.temp_print()
+
+
