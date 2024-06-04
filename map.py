1b2e06854dc4c21a04837d15f2dd294d2a0b5b6e
Moazzem Hossain <48817696+hossainmoazzem@users.noreply.github.com>
2024-06-04 09:19:34 +0000

    One liner if-else inside a for loop

---
+++ map.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+students = [Student("Omar", 0.78), Student("Bayezid", 0.80), Student("Abu Isa", 0.70), Student("Ashraf", 0.90), Student("Mostaq", 0.70), Student("Tutul", 0.65), Student("Osman", 0.66)]
+student_result = []
+#for student in students:
+    # if student.score >= 0.7:
+    #     student_result.append(f"{student.name} Passed")
+    # else:
+    #     student_result.append(f"{student.name} Failed")
+
+[student_result.append(f"{student.name} Passed") if student.score >= 0.7 else student_result.append(f"{student.name} Failed") for student in students]
+
+print(student_result)
